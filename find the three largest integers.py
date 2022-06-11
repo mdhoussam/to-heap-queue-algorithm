1afd529de8008635b49ed7e35153405a2b30e998
mdhoussam <islamhoussam@iut-dhaka.edu>
2022-06-11 20:19:29 +0600

    find the small

--- find the three largest integers.py
+++ find the three largest integers.py
@@ -3,5 +3,10 @@ nums_list = [25, 35, 22, 85, 14, 65, 75, 22, 58]
 print("Original list:")
 print(nums_list)
 # Find three largest values
-largest_nums = hq.nlargest(3, nums_list)
-print("\nThree largest numbers are:", largest_nums)
+#largest_nums = hq.nlargest(3, nums_list)
+#print("\nThree largest numbers are:", largest_nums)
+
+# Find three smallest values
+largest_nums = hq.nsmallest(3, nums_list)
+print("\nThree smallest numbers are:", largest_nums)
+
