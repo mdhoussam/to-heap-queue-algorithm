14eb9219c992ebbbe82c5d2ad1a5d7909ee1e7db
mdhoussam <islamhoussam@iut-dhaka.edu>
2022-06-16 14:07:45 +0600

    max product
    nice

---
+++ 8. Write a Python program to compute maximum product of three numbers of a given array of integers.py
@@ -0,0 +1,9 @@
+def maximumProduct(nums):
+    import heapq
+    a, b = heapq.nlargest(3, nums), heapq.nsmallest(2, nums)
+    return max(a[0] * a[1] * a[2], a[0] * b[0] * b[1])
+arr_nums = [12, 74, 9, 50, 61, 41]
+print("Original array elements:")
+print(arr_nums)
+print("Maximum product of three numbers of the said array:")
+print(maximumProduct(arr_nums))
